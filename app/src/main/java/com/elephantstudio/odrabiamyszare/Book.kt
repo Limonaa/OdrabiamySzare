d74231e73353407b79bf886ce1d4e67430a57286
Limona <mateusz.walaszek@op.pl>
2022-10-23 15:23:34 +0200

    add recyclerView add book data class add new colors

---
+++ app/src/main/java/com/elephantstudio/odrabiamyszare/Book.kt
@@ -0,0 +1,7 @@
+package com.elephantstudio.odrabiamyszare
+
+data class Book(
+    val title: String,
+    val type: String,
+    val publish: String
+)
--- app/src/main/java/com/elephantstudio/odrabiamyszare/FirstFragment.kt
+++ app/src/main/java/com/elephantstudio/odrabiamyszare/FirstFragment.kt
@@ -6,17 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.elephantstudio.odrabiamyszare.databinding.FragmentFirstBinding
+import com.elephantstudio.odrabiamyszare.databinding.ItemBookBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(R.layout.fragment_first) {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +21,19 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+
+        var booksList = mutableListOf(
+            Book("MateMatyka 2. Zakres rozszerzony", "Podręcznik", "Nowa Era"),
+            Book("Język polski w urzyciu 3.1 Zakres podstawowy", "Podręcznik", "GWO"),
+            Book("Oblicza geografii 2. Zakres podstawowy", "Podręcznik", "WSiP"),
+        )
+
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        val adapter = BookAdapter(booksList)
+        binding.rvBooks.adapter = adapter
+        binding.rvBooks.layoutManager = LinearLayoutManager(requireContext())
+
         return binding.root
 
     }
@@ -32,9 +41,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
